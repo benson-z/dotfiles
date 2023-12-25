d9528f0a8c4cefc6a1e5d6aec7a7e9917cb7ee68
Benson Zhou <bensonzhou@yahoo.com>
2023-12-25 01:03:33 -0800

    migrate configs to lazy

---
+++ .config/nvim/lua/benson/plugins/nvim_tree.lua
@@ -0,0 +1,33 @@
+return {
+    "nvim-tree/nvim-tree.lua", 
+    dependencies = {
+        "nvim-tree/nvim-web-devicons"
+    }, 
+    config = function() 
+        -- disable netrw at the very start of your init.lua
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+
+        -- OR setup with some options
+        require("nvim-tree").setup({
+            sort = {
+                sorter = "case_sensitive",
+            },
+            view = {
+                width = 35,
+            },
+            renderer = {
+                group_empty = true,
+            },
+            filters = {
+                git_ignored = false,
+                dotfiles = true,
+            },
+        })
+
+        -- keymaps
+        local api = require("nvim-tree.api")
+
+        vim.keymap.set("n", "<leader>s", api.tree.toggle, {})
+    end
+}
